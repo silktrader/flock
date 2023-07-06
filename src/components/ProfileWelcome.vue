fcb2a68d7c2de1462500ea04e54b7b187fa94e31
Benjamin-Barda <benjamin.barda@hcl.com>
2023-07-06 02:57:27 +0200

    Certo

--- src/components/ProfileWelcome.vue
+++ src/components/ProfileWelcome.vue
@@ -7,7 +7,7 @@
         {{ userName }}
       </div>
 
-      <div class="justify-center row" style="background-color:#822433; width:100vw;">
+      <div class="justify-center row" style="background-color:#2a331e; width:100vw;">
         <q-avatar class='avatar' size="30vh">
           <img :src="profilePictureURL">
         </q-avatar>
@@ -88,7 +88,7 @@ import { ref } from 'vue'
 const profilePictureURL = 'https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg'
 const userName = 'Esteban hugo de la rosa immediata'
 
-const userBio = 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
+const userBio = 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\''
 
 const ratingModel = ref(4)
 
